8a97df720cb2e297681de02256ccbbff8f6e9f04
adityaravichander <aditya.ravichander@gmail.com>
2019-09-04 23:28:31 +0530

    changed P to PID+ proper tuned constants

--- theta_control_3dof.m
+++ theta_control_3dof.m
@@ -22,30 +22,61 @@ theta1_req= (theta12diff+theta12summ)/2
 theta2_req= (theta12diff-theta12summ)/2
 theta3_req= acos(y/(l1*cos(theta1_req)+l2*cos(theta2_req)))
 
-kp = 1.2;
+%declaring pid constants and increment variables
+kp = 0.20;
+ki = 0.5;
+kd = 0.1;
 i = 1; 
-N = 14;
+N = 50;
 
+%calculating all the error variables
 error_theta = zeros(3,N);
 delta_theta = zeros(3,N);
 instant_theta = zeros(3,N);
+sum_error_theta = zeros(3,N);
+dif_error_theta = zeros(3,N);
 
+
+%loop to reach setpoint
 while i < N
     
+	% set theta1
     error_theta(1,i+1)   = theta1_req-instant_theta(1,i) ;
-    delta_theta(1,i+1)   = kp*error_theta(1,i+1);
+    sum_error_theta(1,1) = error_theta(1,1);
+    for v=1:+1:i
+        sum_error_theta(1,v+1) = sum_error_theta(1,v) + error_theta(1,v+1);
+        dif_error_theta(1,v+1) = error_theta(1,v+1) - error_theta(1,v);
+    end 
+    delta_theta(1,i+1)   = kp*error_theta(1,i+1) + ki*sum_error_theta(1,i+1) + kd*dif_error_theta(1,i+1);    
     instant_theta(1,i+1) = instant_theta(1,i) + delta_theta(1,i+1);  
     
+    	% set theta2
+    error_theta(2,i+1)   = theta2_req-instant_theta(2,i) ;sum_error_theta(1,1) = error_theta(1,1);    
+    sum_error_theta(2,1) = error_theta(2,1);
+    for v=1:+1:i
+        sum_error_theta(2,v+1) = sum_error_theta(2,v) + error_theta(2,v+1);
+        dif_error_theta(2,v+1) = error_theta(2,v+1) - error_theta(2,v);
+    end 
+    delta_theta(2,i+1)   = kp*error_theta(2,i+1) + ki*sum_error_theta(2,i+1) + kd*dif_error_theta(2,i+1);    
     
-    error_theta(2,i+1)   = theta2_req-instant_theta(2,i) ;
-    delta_theta(2,i+1)   = kp*error_theta(2,i+1);
     instant_theta(2,i+1) = instant_theta(2,i) + delta_theta(2,i+1); 
     
-    
+    	% set theta3
     error_theta(3,i+1)   = theta3_req-instant_theta(3,i) ;
-    delta_theta(3,i+1)   = kp*error_theta(3,i+1);
+    sum_error_theta(3,1) = error_theta(3,1);
+    for v=1:+1:i
+        sum_error_theta(3,v+1) = sum_error_theta(3,v) + error_theta(3,v+1);
+        dif_error_theta(3,v+1) = error_theta(3,v+1) - error_theta(3,v);
+    end 
+    delta_theta(3,i+1)   = kp*error_theta(3,i+1)+ ki*sum_error_theta(3,i+1) + kd*dif_error_theta(3,i+1);
     instant_theta(3,i+1) = instant_theta(3,i) + delta_theta(3,i+1); 
     
+    
+    % plot the instantaneous values 
     plot(instant_theta(3,:))
+    
     i = i+1;
 end   
+
+
+
