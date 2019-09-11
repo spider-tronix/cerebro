66802c4db19a28d3f96a611d57b0645b1753998b
adityaravichander <aditya.ravichander@gmail.com>
2019-09-11 16:51:19 +0530

    Matlab,ROS integrated

--- Simulation/New_arm/ROS scripts/Thetapublish.m
+++ Simulation/New_arm/ROS scripts/Thetapublish.m
@@ -1,53 +1,60 @@
 rosinit;
+
 %Publisher  publishing  to the control theta 
 %subscriber subscribing to simulation theta values
 %custom message file simulation/theta
-pub_name=rospublisher("/controltheta","simulation/theta");
-sub_name=rossubscriber("/thetafeedback",@callbacktheta)
+
+pub_name = rospublisher('/controltheta','catkin_ws/theta');
+
+% sub_name=rossubscriber("/thetafeedback",@callbacktheta)
+
 pause(2)
+
 %Creating a ROS msg holder 
 theta=rosmessage(pub_name);
 
-%Callback function for theta feedback from Gazebo
-function callbacktheta(msg)
-    xyz=msg.position
-end
+%declaring variables
+theta_req = zeros(1,3);
+error_theta = zeros(1,3);
+error_prev = zeros(1,3);
+delta_theta = zeros(1,3);
+instant_theta = zeros(1,3);
+sum_error_theta = zeros(1,3);
+dif_error_theta = zeros(1,3);
 
-while true:
-         % set theta1
-        error_theta(1,i+1)   = theta1_req-instant_theta(1,i) ;
-        sum_error_theta(1,1) = error_theta(1,1);
-        for v=1:+1:i
-            sum_error_theta(1,v+1) = sum_error_theta(1,v) + error_theta(1,v+1);
-            dif_error_theta(1,v+1) = error_theta(1,v+1) - error_theta(1,v);
-        end 
-        delta_theta(1,i+1)   = kp*error_theta(1,i+1) + ki*sum_error_theta(1,i+1) + kd*dif_error_theta(1,i+1);    
-        instant_theta(1,i+1) = instant_theta(1,i) + delta_theta(1,i+1);  
-        
-         % set theta2
-        error_theta(2,i+1)   = theta2_req-instant_theta(2,i) ;sum_error_theta(1,1) = error_theta(1,1);    
-        sum_error_theta(2,1) = error_theta(2,1);
-        for v=1:+1:i
-            sum_error_theta(2,v+1) = sum_error_theta(2,v) + error_theta(2,v+1);
-            dif_error_theta(2,v+1) = error_theta(2,v+1) - error_theta(2,v);
-        end 
-        delta_theta(2,i+1)   = kp*error_theta(2,i+1) + ki*sum_error_theta(2,i+1) + kd*dif_error_theta(2,i+1);    
-        
-        instant_theta(2,i+1) = instant_theta(2,i) + delta_theta(2,i+1); 
-        
-         % set theta3
-        error_theta(3,i+1)   = theta3_req-instant_theta(3,i) ;
-        sum_error_theta(3,1) = error_theta(3,1);
-        for v=1:+1:i
-            sum_error_theta(3,v+1) = sum_error_theta(3,v) + error_theta(3,v+1);
-            dif_error_theta(3,v+1) = error_theta(3,v+1) - error_theta(3,v);
-        end 
-        delta_theta(3,i+1)   = kp*error_theta(3,i+1)+ ki*sum_error_theta(3,i+1) + kd*dif_error_theta(3,i+1);
-        instant_theta(3,i+1) = instant_theta(3,i) + delta_theta(3,i+1);
+%declaring pid constants and increment variables
+kp = 0.20;
+ki = 0.5;
+kd = 0.1;
 
-        theta.theta1=         %place for input for theta1
-        theta.theta2=         %place for input for theta2
-        theta.theta3=         %place for input for theta3
-        send(pub_name,theta
+
+while true
+
+     
+    for i=1:+1:3
+        error_theta(1,i)   = theta_req(1,i)-instant_theta(1,i) ;    
+    end
+     
+    for i=1:+1:3
+        sum_error_theta(1,i) = sum_error_theta(1,i) + error_theta(1,i);       
+        dif_error_theta(1,i) = error_theta(1,i) - error_prev(1,i);
+    end 
+    
+    for i=1:+1:3
+        delta_theta(1,i)   = kp*error_theta(1,i) + ki*sum_error_theta(1,i) + kd*dif_error_theta(1,i);         
+    end
+    
+    for i=1:+1:3
+        error_prev(1,i)   = error_theta(1,i) ;    
+    end
+    theta.Data = 'A';         %place for input for theta1
+%   theta.theta2=         %place for input for theta2
+%   theta.theta3=         %place for input for theta3
+    send(pub_name,theta)
 end 
         
+% Callback function for theta feedback from Gazebo
+% function callbacktheta(msg)
+%     xyz=msg.position
+% end
+
--- theta_control_3dof.m
+++ theta_control_3dof.m
@@ -2,7 +2,6 @@
 
 l1=0.2;
 l2=0.2;  %IDEAL CASE
-
 L=sqrt((l1^2)+(l2^2));
 
 x=0;
@@ -16,67 +15,44 @@ t=sqrt(x^2+y^2);
 theta12diff= acos((z^2+t^2-L^2)/(2*l1*l2));
 theta12summ= acos((z^2-t^2-L^2)/(2*l1*l2));
 
-%Individual angle obtained using arccosine
-
-theta1_req= (theta12diff+theta12summ)/2
-theta2_req= (theta12diff-theta12summ)/2
-theta3_req= acos(y/(l1*cos(theta1_req)+l2*cos(theta2_req)))
-
 %declaring pid constants and increment variables
 kp = 0.20;
 ki = 0.5;
 kd = 0.1;
-i = 1; 
-N = 50;
 
 %calculating all the error variables
-error_theta = zeros(3,N);
-delta_theta = zeros(3,N);
-instant_theta = zeros(3,N);
-sum_error_theta = zeros(3,N);
-dif_error_theta = zeros(3,N);
+theta_req = zeros(1,3);
+error_theta = zeros(1,3);
+error_prev = zeros(1,3);
+delta_theta = zeros(1,3);
+instant_theta = zeros(1,3);
+sum_error_theta = zeros(1,3);
+dif_error_theta = zeros(1,3);
 
+%Individual angle obtained using arccosine
 
-%loop to reach setpoint
-while i < N
+theta_req(1,1)= (theta12diff+theta12summ)/2;
+theta_req(1,2)= (theta12diff-theta12summ)/2;
+theta_req(1,3)= acos(y/(l1*cos(theta_req(1,1))+l2*cos(theta_req(1,2))));
+
+while true
     
-	% set theta1
-    error_theta(1,i+1)   = theta1_req-instant_theta(1,i) ;
-    sum_error_theta(1,1) = error_theta(1,1);
-    for v=1:+1:i
-        sum_error_theta(1,v+1) = sum_error_theta(1,v) + error_theta(1,v+1);
-        dif_error_theta(1,v+1) = error_theta(1,v+1) - error_theta(1,v);
+    for i=1:+1:3
+        error_theta(1,i)   = theta_req(1,i)-instant_theta(1,i) ;    
+    end
+     
+    for i=1:+1:3
+        sum_error_theta(1,i) = sum_error_theta(1,i) + error_theta(1,i);       
+        dif_error_theta(1,i) = error_theta(1,i) - error_prev(1,i);
     end 
-    delta_theta(1,i+1)   = kp*error_theta(1,i+1) + ki*sum_error_theta(1,i+1) + kd*dif_error_theta(1,i+1);    
-    instant_theta(1,i+1) = instant_theta(1,i) + delta_theta(1,i+1);  
     
-    	% set theta2
-    error_theta(2,i+1)   = theta2_req-instant_theta(2,i) ;sum_error_theta(1,1) = error_theta(1,1);    
-    sum_error_theta(2,1) = error_theta(2,1);
-    for v=1:+1:i
-        sum_error_theta(2,v+1) = sum_error_theta(2,v) + error_theta(2,v+1);
-        dif_error_theta(2,v+1) = error_theta(2,v+1) - error_theta(2,v);
-    end 
-    delta_theta(2,i+1)   = kp*error_theta(2,i+1) + ki*sum_error_theta(2,i+1) + kd*dif_error_theta(2,i+1);    
+    for i=1:+1:3
+        delta_theta(1,i)   = kp*error_theta(1,i) + ki*sum_error_theta(1,i) + kd*dif_error_theta(1,i);         
+    end
     
-    instant_theta(2,i+1) = instant_theta(2,i) + delta_theta(2,i+1); 
+    for i=1:+1:3
+        error_prev(1,i)   = error_theta(1,i) ;    
+    end
     
-    	% set theta3
-    error_theta(3,i+1)   = theta3_req-instant_theta(3,i) ;
-    sum_error_theta(3,1) = error_theta(3,1);
-    for v=1:+1:i
-        sum_error_theta(3,v+1) = sum_error_theta(3,v) + error_theta(3,v+1);
-        dif_error_theta(3,v+1) = error_theta(3,v+1) - error_theta(3,v);
-    end 
-    delta_theta(3,i+1)   = kp*error_theta(3,i+1)+ ki*sum_error_theta(3,i+1) + kd*dif_error_theta(3,i+1);
-    instant_theta(3,i+1) = instant_theta(3,i) + delta_theta(3,i+1); 
-    
-    
-    % plot the instantaneous values 
-    plot(instant_theta(3,:))
-    
-    i = i+1;
 end   
-
-
 
